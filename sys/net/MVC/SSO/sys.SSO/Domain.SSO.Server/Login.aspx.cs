46f514e88b56be6a24ac78e2c4c19371349cf6c5
songjiang <2421542678@qq.com>
2019-09-18 17:41:56 +0800

    sso

---
+++ sys/net/MVC/SSO/sys.SSO/Domain.SSO.Server/Login.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Domain.SSO.Server
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string userName = this.txtUserName.Text.Trim();
+            string password = this.txtPassword.Text.Trim();
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                this.lblMessage.Text = "用户名不能为空";
+                return;
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                this.lblMessage.Text = "密码不能为空";
+                return;
+            }
+
+            if (Domain.Security.SmartAuthenticate.AuthenticateUser(userName, password, true))
+            {
+                this.lblMessage.Text = "登录成功";
+                string returnUrl = Request["returnUrl"];
+                if (string.IsNullOrEmpty(returnUrl))
+                    Response.Redirect("default.aspx");
+                else
+                    Response.Redirect(returnUrl);
+            }
+            else
+            {
+                this.lblMessage.Text = "登录失败，请重试";
+                return;
+            }
+        }
+    }
+}
---
+++ sys/net/MVC/SSO/sys.SSO/Domain.Security/LoginUserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Domain.Security
+{
+    public class LoginUserModel
+    {
+        public string UserName { get; set; }
+        public string LoginTime { get; set; }
+    }
+}
